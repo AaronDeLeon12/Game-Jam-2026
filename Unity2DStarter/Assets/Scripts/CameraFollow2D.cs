a58f44bba9070549778d161f3b936430e8d04c18
AaronDeLeon <a00843681@tec.mx>
2026-05-15 23:12:18 -0600

    Tune camera follow for gameplay

--- Unity2DStarter/Assets/Scripts/CameraFollow2D.cs
+++ Unity2DStarter/Assets/Scripts/CameraFollow2D.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class CameraFollow2D : MonoBehaviour
 {
     [SerializeField] private Transform target;
-    [SerializeField] private float followSpeed = 8f;
-    [SerializeField] private Vector3 offset = new Vector3(0f, 1f, -10f);
+    [SerializeField] private float followSpeed = 4.5f;
+    [SerializeField] private Vector3 offset = new Vector3(0f, 2.5f, -10f);
 
     public void SetTarget(Transform newTarget)
     {
